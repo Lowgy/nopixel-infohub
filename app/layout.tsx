d6956e5f1ee77486694a9fb1e1152c27c69860f1
Logan Olson <loganolsonn@gmail.com>
2024-01-26 19:24:04 -0600

    Added vercel/analytics for fun, changed naming, created streams page folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css';
 
 import { SiteHeader } from '@/components/site-header';
 import { SiteFooter } from '@/components/site-footer';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -23,7 +24,10 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className="relative flex min-h-screen flex-col">
           <SiteHeader />
-          <div className="flex-1">{children}</div>
+          <div className="flex-1">
+            {children}
+            <Analytics />
+          </div>
           <SiteFooter />
         </div>
       </body>
